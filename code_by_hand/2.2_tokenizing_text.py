603517a4b90568bf4c4ffa9dd6d5545c1e52d95b
Jagger_server <56354235+jagger235711@users.noreply.github.com>
2026-03-05 15:59:34 +0800

    Refactor dataloader and tokenizer implementation
    - Updated execution count in Jupyter notebook to null for reproducibility.
    - Improved code readability by adjusting spacing in input and target chunk assignments.
    - Refactored the `create_dataloader_v1` function for better parameter formatting.
    - Added new tokenization examples in `2.2_tokenizing_text.py` to demonstrate usage of tiktoken.
    - Enhanced the dataset class to handle input and target tensors more efficiently.
    - Updated `pyproject.toml` to include additional dependencies for networking and transformers.

--- code_by_hand/2.2_tokenizing_text.py
+++ code_by_hand/2.2_tokenizing_text.py
@@ -128,3 +128,140 @@ text1 = "Hello, do you like tea?"
 text2 = "In the sunlit terraces of the palace."
 text = " <|endoftext|> ".join((text1, text2))
 print(text)
+
+# %% tiktoken子词分词，socket不可用
+import tiktoken
+
+# from transformers import GPT2Tokenizer
+
+
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+    "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+# %%
+strings = tokenizer.decode(integers)
+print(strings)
+# %%
+text = "Akwirw ier"
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
+
+# %%
+with open(articlePath, "r", encoding="utf-8") as f:
+    raw_text = f.read()
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+# %%
+enc_sample = enc_text[50:]
+
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1 : context_size + 1]
+
+print(f"x: {x}")
+print(f"y:           {y}")
+
+for i in range(1, context_size + 1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(context, "---->", desired)
+# %%
+for i in range(1, context_size + 1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+# %%
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride) -> None:
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i : i + max_length]
+            target_chunk = token_ids[i + 1 : i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+# %%
+def create_dataloader_v1(
+    txt,
+    batch_size=4,
+    max_length=256,
+    stride=128,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+    return dataloader
+
+
+# %%
+with open(articlePath, "r", encoding="utf-8") as f:
+    raw_text = f.read()
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
+    )
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+    print(first_batch)
+
+# %% 不同参数的基于滑动窗口的数据加载器
+with open(articlePath, "r", encoding="utf-8") as f:
+    raw_text = f.read()
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=2, max_length=4, stride=3, shuffle=False
+    )
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+    print(first_batch)
+    print("-------------------")
+    print("                   " + str(next(data_iter)))
+    print("-------------------")
+    print("                   " + str(next(data_iter)))
+
+# %% [markdown]
+
+# 单个batch会输出两个tenser,后一个tensor是前一个的预测目标
+
+# [tensor([[  40,  367, 2885, 1464],[1464, 1807, 3619,  402]]),
+#            tensor([[ 367, 2885, 1464, 1807],[1807, 3619,  402,  271]])]
+# -------------------
+#                    [tensor([[  402,   271, 10899,  2138],
+#         [ 2138,   257,  7026, 15632]]), tensor([[  271, 10899,  2138,   257],
+#         [  257,  7026, 15632,   438]])]
+# -------------------
+#                    [tensor([[15632,   438,  2016,   257],
+#         [  257,   922,  5891,  1576]]), tensor([[ 438, 2016,  257,  922],
+#         [ 922, 5891, 1576,  438]])]
