c6571668a3f81c890ad172c4d0e4878d3414d3cd
Jagger_server <56354235+jagger235711@users.noreply.github.com>
2026-04-14 16:33:54 +0800

    添加生成函数，支持温度缩放和top-k选择，增强文本生成的灵活性

--- code_by_hand/ch5_pretraining.py
+++ code_by_hand/ch5_pretraining.py
@@ -402,4 +402,67 @@ for index, probas in enumerate(scaled_probas):
     print("温度：%d，概率：%f" % (index, p[token_index]))
 
 # %%
-# test
+top_k = 3
+top_logits, top_pos = torch.topk(next_token_logits, top_k)
+print("Top logits:", top_logits)
+print("Top positions:", top_pos)
+
+# %%
+new_logits = torch.where(
+    condition=next_token_logits < top_logits[-1],
+    input=torch.tensor(float("-inf")),
+    other=next_token_logits,
+)
+print(new_logits)
+
+# %%
+topk_probas = torch.softmax(new_logits, dim=0)
+print(topk_probas)
+
+
+# %%
+def generate(
+    model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None
+):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits
+            )
+        if temperature > 0.0:  # 温度缩放
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:  # 直接取最大概率的token
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        if idx_next == eos_id:
+            break
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+
+# %%
+torch.manual_seed(123)
+token_ids = generate(
+    model=model,
+    idx=text_to_token_ids("Every effort moves you", tokenizer),
+    max_new_tokens=15,
+    context_size=GPT_CONFIG_124M["context_length"],
+    top_k=25,
+    temperature=1.4,
+)
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+# %%练习5.2
+# 强调文本准确性的生成可以采用较低的温度（如0.7或更低）和较小的top_k值（如10或更小），以确保生成的文本更倾向于选择概率较高的词汇，从而提高文本的准确性和连贯性。例如给定知识库问答
+
+# %%练习5.3
+# 将温度设置的非常小
+
+# %%5.4
