e39c26e491e4feff65cb8f50502da5a1fa6a28b9
Jagger_server <56354235+jagger235711@users.noreply.github.com>
2026-04-22 16:29:33 +0800

    添加GPT模型权重加载功能，更新生成函数以支持Top-k选择和温度参数，增强文本生成效果

--- code_by_hand/ch5_pretraining.py
+++ code_by_hand/ch5_pretraining.py
@@ -421,6 +421,11 @@ print(topk_probas)
 
 
 # %%
+# GPT 预测下一个词时，会输出一个包含所有词汇概率的列表（logits）。
+
+
+# 不加 Top-k：从所有词里挑，可能选出一个很生僻但概率略高的词，导致文不对题。
+# 加上 Top-k：只保留概率最高的前 k 个词，把剩下所有词的概率直接变成 −∞（负无穷）。
 def generate(
     model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None
 ):
@@ -504,6 +509,135 @@ from gpt_download import download_and_load_gpt2
 settings, params = download_and_load_gpt2(model_size="124M", models_dir="gpt2")
 
 # %%
-print("Settings:", settings) 
+print("Settings:", settings)
 print("Parameter dictionary keys:", params.keys())
+# %%
+print(params["wte"])
+print("Token embedding weight tensor dimensions:", params["wte"].shape)
+
+# %%
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+# %%
+model_name = "gpt2-small (124M)"
+NEW_CONFIG = GPT_CONFIG_124M.copy()  # 这个124M是单个配置
+# print(NEW_CONFIG)
+NEW_CONFIG.update(
+    model_configs[model_name]
+)  # 把model_configs[model_name]中的键值对更新到NEW_CONFIG中
+# print(NEW_CONFIG)
+NEW_CONFIG.update({"context_length": 1024})
+# print(NEW_CONFIG)
+NEW_CONFIG.update({"qkv_bias": True})
+# %%
+gpt = GPTModel(NEW_CONFIG)
+gpt.eval()
+
+
+# %%
+def assign(left, right):
+    # 检查两个张量或数组(left 和 right) 是否具有相同的维度或形状,并返回可训练的 PyTorch 参数
+
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left: {left.shape}, " "Right: {right.shape}")
+    return torch.nn.Parameter(torch.tensor(right))
+
+
+# %%
+import numpy as np
+
+
+def load_weights_into_gpt(gpt, params):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params["wpe"])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params["wte"])
+    for b in range(len(params["blocks"])):
+        # 在每个block中，把每个layer的权重和偏置都加载到GPT模型中
+        q_w, k_w, v_w = np.split(  # q,k,v
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1
+        )
+        gpt.trf_blocks[b].att.W_query.weight = assign(
+            gpt.trf_blocks[b].att.W_query.weight, q_w.T
+        )
+        gpt.trf_blocks[b].att.W_key.weight = assign(
+            gpt.trf_blocks[b].att.W_key.weight, k_w.T
+        )
+        gpt.trf_blocks[b].att.W_value.weight = assign(
+            gpt.trf_blocks[b].att.W_value.weight, v_w.T
+        )
+
+        q_b, k_b, v_b = np.split(  # bias
+            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1
+        )
+        gpt.trf_blocks[b].att.W_query.bias = assign(
+            gpt.trf_blocks[b].att.W_query.bias, q_b
+        )
+        gpt.trf_blocks[b].att.W_key.bias = assign(gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = assign(
+            gpt.trf_blocks[b].att.W_value.bias, v_b
+        )
+
+        # 输出层
+        gpt.trf_blocks[b].att.out_proj.weight = assign(
+            gpt.trf_blocks[b].att.out_proj.weight,
+            params["blocks"][b]["attn"]["c_proj"]["w"].T,
+        )
+        gpt.trf_blocks[b].att.out_proj.bias = assign(
+            gpt.trf_blocks[b].att.out_proj.bias,
+            params["blocks"][b]["attn"]["c_proj"]["b"],
+        )
+        # ffn
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight,
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T,
+        )
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias, params["blocks"][b]["mlp"]["c_fc"]["b"]
+        )
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight,
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T,
+        )
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias,
+            params["blocks"][b]["mlp"]["c_proj"]["b"],
+        )
+        # norm
+        gpt.trf_blocks[b].norm1.scale = assign(
+            gpt.trf_blocks[b].norm1.scale, params["blocks"][b]["ln_1"]["g"]
+        )
+        gpt.trf_blocks[b].norm1.shift = assign(
+            gpt.trf_blocks[b].norm1.shift, params["blocks"][b]["ln_1"]["b"]
+        )
+        gpt.trf_blocks[b].norm2.scale = assign(
+            gpt.trf_blocks[b].norm2.scale, params["blocks"][b]["ln_2"]["g"]
+        )
+        gpt.trf_blocks[b].norm2.shift = assign(
+            gpt.trf_blocks[b].norm2.shift, params["blocks"][b]["ln_2"]["b"]
+        )
+    #   最后一层归一化 + 输出头
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])  # 权重绑定
+
+
+# %%
+load_weights_into_gpt(gpt, params)
+gpt.to(device)
+# %%
+torch.manual_seed(123)
+token_ids = generate(
+    model=gpt,
+    idx=text_to_token_ids("Every effort moves you", tokenizer).to(device),
+    max_new_tokens=25,
+    context_size=NEW_CONFIG["context_length"],
+    top_k=50,  # 候选范围
+    temperature=1.5,  # 随机性
+)
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
 # %%
