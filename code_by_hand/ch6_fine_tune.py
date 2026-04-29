763adff9bc2e8051980bd326919660e4a7f5c74e
Jagger_server <56354235+jagger235711@users.noreply.github.com>
2026-04-29 14:29:38 +0800

    新增模型配置和生成文本功能，更新生成函数以支持温度和Top-k采样，重构训练和评估流程

--- code_by_hand/ch6_fine_tune.py
+++ code_by_hand/ch6_fine_tune.py
@@ -147,3 +147,94 @@ print(f"{len(val_loader)} validation batches")
 print(f"{len(test_loader)} test batches")
 
 # %%
+CHOOSE_MODEL = "gpt2-small (124M)"
+INPUT_PROMPT = "Every effort moves"
+BASE_CONFIG = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "drop_rate": 0.0,
+    "qkv_bias": True,
+}
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+# %%
+from gpt_download import download_and_load_gpt2
+from previous_chapters import GPTModel, load_weights_into_gpt
+
+model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+settings, params = download_and_load_gpt2(model_size=model_size, models_dir="gpt2")
+model = GPTModel(BASE_CONFIG)
+load_weights_into_gpt(model, params)
+model.eval()
+
+# %%
+from previous_chapters import generate_text_simple
+from previous_chapters import text_to_token_ids, token_ids_to_text
+
+text_1 = "Every effort moves you"
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids(text_1, tokenizer),
+    max_new_tokens=15,
+    context_size=BASE_CONFIG["context_length"],
+)
+print(token_ids_to_text(token_ids, tokenizer))
+
+# %% 通过用指令提示模型 来让模型已经对垃圾邮件进行分类
+text_2 = (
+    "Is the following text 'spam'? Answer with 'yes' or 'no':"
+    " 'You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award.'"
+)
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids(text_2, tokenizer),
+    max_new_tokens=23,
+    context_size=BASE_CONFIG["context_length"],
+)
+print(token_ids_to_text(token_ids, tokenizer))
+
+# %%
+print(model)  # 查看模型结构以修改模型
+# %%冻结模型所有参数
+for param in model.parameters():
+    param.requires_grad = False
+
+# %%
+torch.manual_seed(123)
+num_classes = 2
+model.out_head = torch.nn.Linear(
+    in_features=BASE_CONFIG["emb_dim"], out_features=num_classes
+)
+# %%将最终的layernorm和transformer块加入训练
+for param in model.trf_blocks[-1].parameters():
+    param.requires_grad = True
+for param in model.final_norm.parameters():
+    param.requires_grad = True
+
+# %%
+inputs = tokenizer.encode("Do you have time")
+inputs = torch.tensor(inputs).unsqueeze(0)
+print("Inputs:", inputs)
+print("Inputs dimensions:", inputs.shape)
+
+# %%
+with torch.no_grad():  # 阻止构建计算图
+    outputs = model(inputs)
+print("Outputs:\n", outputs)
+print("Outputs dimensions:", outputs.shape)  # [1,4,2]  batch_size,num_tokens,emb_dim
+# %%
+# 序列中的最后一个标记积累了最多信息,因为 它唯一能够访问所有先前标记的数据。因此,在我们的垃圾邮件分类任务中,我们在 微调过程中关注这个最后一个标记。
+first_token=outputs[:,0,:]
+last_token = outputs[:, -1, :]
+
+print("first output token:", first_token)
+print("Last output token:", last_token)
+
+# %%
