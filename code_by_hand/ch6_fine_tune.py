d24273552972583e7d2c4cccb1427263b5dac3bf
Jagger_server <56354235+jagger235711@users.noreply.github.com>
2026-04-30 17:39:25 +0800

    新增评论分类功能，添加文本分类函数和示例，保存和加载模型状态

--- code_by_hand/ch6_fine_tune.py
+++ code_by_hand/ch6_fine_tune.py
@@ -477,4 +477,49 @@ print(
 )  # 验证集的精度相较训练集会高一点。因为我们是针对验证集进行的调参
 print(f"Test accuracy: {test_accuracy*100:.2f}%")
 
+
+# %%
+def classify_review(
+    text, model, tokenizer, device, max_length=None, pad_token_id=50256
+):
+    model.eval()
+
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[1]
+    input_ids = input_ids[: min(max_length, supported_context_length)]  # 序列过长就截断
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0)
+    with torch.no_grad():
+        logits = model(input_tensor)[:, -1, :]
+    predicted_label = torch.argmax(logits, dim=-1).item()
+    return "spam" if predicted_label == 1 else "not spam"
+
+
+# %%
+text_1 = (
+    "You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award."
+)
+print(
+    classify_review(
+        text_1, model, tokenizer, device, max_length=train_dataset.max_length
+    )
+)
+
+# %%
+text_2 = (
+    "Hey, just wanted to check if we're still on" " for dinner tonight? Let me know!"
+)
+print(
+    classify_review(
+        text_2, model, tokenizer, device, max_length=train_dataset.max_length
+    )
+)
+
+# %%
+torch.save(model.state_dict(), "review_classifier.pth")
+
+# %%
+model_state_dict = torch.load("review_classifier.pth", map_location=device) 
+model.load_state_dict(model_state_dict)
 # %%
